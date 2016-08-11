bf3c65111cb00de15f983ee7ed26dcd2c1493ba4
kaushalpatel922 <kaushalpatel922@gmail.com>
2016-08-10 20:07:21 -0400

    seconds counter on the page working

--- timer/timer/src/App.js
+++ timer/timer/src/App.js
@@ -2,20 +2,40 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+var App = React.createClass({
+  getInitialState: function() {
+    return {
+      secondsEnded: 0
+    }
+  },
+  startCounting: function() {
+    this.setState({
+      secondsEnded: this.state.secondsEnded + 1
+    })
+  },
+  start: function() {
+    this.interval = setInterval(this.startCounting, 1000);
+  },
+  stop: function() {
+    clearInterval(this.interval);
+  },
+  reset: function () {
+    this.setState({
+      secondsEnded: 0
+    })
+  },
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Timer</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>You've been on this page since last {this.state.secondsEnded} seconds.</p>
+        <button className='button' onClick={this.stop}>Stop</button><button className='button' onClick={this.start}>Start</button><button className='button' onClick={this.reset}>Reset</button>
       </div>
     );
   }
-}
+})
 
-export default App;
+module.exports = App;
